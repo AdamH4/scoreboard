7f547d203a8de221a8f2bd852e908738001713f1
Adam Harnusek <adam.harnusek@gmail.com>
2023-08-28 14:46:55 +0200

    create scoreboard summary test with example from task description

---
+++ tests/scoreboardSummary.test.ts
@@ -0,0 +1,62 @@
+import { test, expect, describe, beforeEach} from '@jest/globals'
+import Scoreboard from '../src/scoreboard'
+import Match from '../src/model/match'
+
+
+describe('Scoreboard sumarry', () => {
+
+  let scoreboard: Scoreboard
+
+  const input = [
+    {
+      teams: ['Mexico', 'Canada'],
+      scores: [0, 5],
+      orderIndex: 2,
+    },
+    {
+      teams: ['Spain', 'Brazil'],
+      scores: [10, 2],
+      orderIndex: 1,
+    },
+    {
+      teams: ['Germany', 'France'],
+      scores: [2, 2],
+      orderIndex: 4,
+    },
+    {
+      teams: ['Uruguay', 'Italy'],
+      scores: [6, 6],
+      orderIndex: 0,
+    },
+    {
+      teams: ['Argentina', 'Australia'],
+      scores: [3, 1],
+      orderIndex: 3,
+    },
+  ]
+
+  let outputMatches: Match[] = new Array(input.length)
+
+  beforeEach(async () => {
+    scoreboard = new Scoreboard()
+
+    for(const game of input) {
+      const match = scoreboard.createMatch(game.teams[0], game.teams[1])
+      if(match) {
+        match.updateScore(game.scores[0], game.scores[1])
+        outputMatches.splice(game.orderIndex, 1, match)
+      }
+
+      await new Promise(resolve => setTimeout(resolve, 100))
+    }
+  })
+
+
+  test('Get sumarry', () => {
+    const summary = scoreboard.getSummary()
+
+    expect(summary).toStrictEqual(outputMatches)
+  })
+
+})
+
